c1048429b63e387423c61b4c1d2821943867643e
DragoPrime <99618256+DragoPrime@users.noreply.github.com>
2023-03-21 12:01:59 +0200

    Update image-channel.py

--- image-channel/image-channel.py
+++ image-channel/image-channel.py
@@ -16,10 +16,10 @@ class ImagesOnly(commands.Cog):
         await channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
 
         # Set channel topic to provide instructions to users
-        await channel.edit(topic="This channel is now restricted to image-only messages "
-                                 "and public thread replies. Any non-image messages will be removed. "
-                                 "To respond to an image, please use the 'start thread' button and "
-                                 "post your message as a public reply.")
+        await channel.edit(topic="Acest canal este acum limitat la mesaje doar cu imagini "
+                                 "și răspunsuri publice. Orice mesaje care nu sunt imagini vor fi eliminate. "
+                                 "Pentru a răspunde la o imagine, vă rugăm să utilizați butonul <<începe fir>> și "
+                                 "postați mesajul dvs. ca răspuns public.")
 
         # Define a message filter to remove non-image messages
         def image_filter(message):
@@ -36,8 +36,8 @@ class ImagesOnly(commands.Cog):
         self.bot.add_thread_created_handler(channel, thread_created)
 
         # Confirm the channel update to the user
-        await ctx.send("This channel has been updated to restrict to image-only messages and public thread replies. "
-                       "To undo this and allow regular messages, use !images_only command again.")
+        await ctx.send("Acest canal a fost actualizat pentru a restricționa mesajele doar cu imagini și răspunsuri doar prin fire publice. "
+                       "Pentru a anula acest lucru și a permite mesajele obișnuite, utilizați din nou comanda [p]images_only.")
 
     def cog_unload(self):
         # Remove message filter and thread creation handlers
