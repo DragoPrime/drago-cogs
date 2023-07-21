57a1f7c07df0d50201cfc21cf3f2000a301e8599
DragoPrime <99618256+DragoPrime@users.noreply.github.com>
2023-07-21 19:00:30 +0300

    final step

--- inara/inara.py
+++ inara/inara.py
@@ -5,7 +5,23 @@ class inara(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def inara(self, ctx):
-        """This does stuff!"""
-        # Your code will go here
-        await ctx.send("I can do stuff!")
+    async def inara(self, ctx, image_variable):
+        try:
+            # Replace 'YOUR_URL' with the base URL containing the variable
+            url = f'https://inara.cz/data/sig/400/{image_variable}.jpg'
+            response = requests.get(url)
+
+            # Check if the request was successful
+            if response.status_code == 200:
+                # Get the image file name from the URL (you can adjust this based on your URL structure)
+                file_name = url.split('/')[-1]
+
+                # Create a discord.File object from the image data
+                file = discord.File(response.content, filename=file_name)
+
+                # Send the image as an attachment
+                await ctx.send(file=file)
+            else:
+                await ctx.send("Image not found.")
+        except Exception as e:
+            await ctx.send(f"An error occurred: {e}")
