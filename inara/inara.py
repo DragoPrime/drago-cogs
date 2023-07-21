d75ba5804cdf63f339140304662578d9ebacce0c
DragoPrime <99618256+DragoPrime@users.noreply.github.com>
2023-07-21 19:12:14 +0300

    final 1

--- inara/inara.py
+++ inara/inara.py
@@ -1,6 +1,6 @@
 import discord
 from redbot.core import commands
-import requests
+import aiohttp
 
 class inara(commands.Cog):
     def __init__(self, bot):
@@ -11,19 +11,21 @@ class inara(commands.Cog):
         try:
             # Replace 'YOUR_URL' with the base URL containing the variable
             url = f'https://inara.cz/data/sig/400/{image_variable}.jpg'
-            response = requests.get(url)
 
-            # Check if the request was successful
-            if response.status_code == 200:
-                # Get the image file name from the URL (you can adjust this based on your URL structure)
-                file_name = url.split('/')[-1]
+            # Fetch the image data using discord.http
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as response:
+                    if response.status == 200:
+                        # Get the image file name from the URL (you can adjust this based on your URL structure)
+                        file_name = url.split('/')[-1]
 
-                # Create a discord.File object from the image data
-                file = discord.File(response.content, filename=file_name)
+                        # Create a discord.File object from the image data
+                        image_data = await response.read()
+                        file = discord.File(image_data, filename=file_name)
 
-                # Send the image as an attachment
-                await ctx.send(file=file)
-            else:
-                await ctx.send("Image not found.")
+                        # Send the image as an attachment
+                        await ctx.send(file=file)
+                    else:
+                        await ctx.send("Image not found.")
         except Exception as e:
             await ctx.send(f"An error occurred: {e}")
