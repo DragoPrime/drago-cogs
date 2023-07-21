c13265cdbd5a358db951fbb79a4f348aab20e787
DragoPrime <99618256+DragoPrime@users.noreply.github.com>
2023-07-21 18:44:34 +0300

    test

--- inara/inara.py
+++ inara/inara.py
@@ -1,12 +1,11 @@
-import discord
-
-from redbot.core import commands, app_commands
+from redbot.core import commands
 
 class inara(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.command()
-    @app_commands.describe(userid="ID-ul de utilizator de pe Inara.")
-    async def inara(self, interaction: discord.Interaction, id: any):
-        await interaction.response.send_message(f"ID-ul tau este {id.value}", ephemeral=True)
+    @commands.command()
+    async def mycom(self, ctx):
+        """This does stuff!"""
+        # Your code will go here
+        await ctx.send("I can do stuff!")
