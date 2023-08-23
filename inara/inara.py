b34316d57b4852595ff6c0612e2755545c5c77d2
DragoPrime <99618256+DragoPrime@users.noreply.github.com>
2023-08-23 16:34:45 +0300

    Update inara.py

--- inara/inara.py
+++ inara/inara.py
@@ -7,27 +7,26 @@ class inara(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def inara(self, ctx, image_variable):
+    async def inara(self, ctx, image_variable: str):  # Make sure the parameter is properly annotated
         try:
-            # Replace 'YOUR_BASE_URL' with the base URL containing the variable
             base_url = 'https://inara.cz/data/sig/400/'
             url = f'{base_url}{image_variable}.jpg'
 
-            # Fetch the image data using aiohttp
             async with aiohttp.ClientSession() as session:
                 async with session.get(url) as response:
                     if response.status == 200:
-                        # Check if the response content type is an image
+                        # Check the actual content type
                         content_type = response.headers.get("Content-Type", "")
                         if not content_type.startswith("image"):
                             await ctx.send("The URL does not point to an image.")
                             return
 
-                        # Send the image as an attachment
                         image_data = await response.read()
                         filename = f"{image_variable}.jpg"
                         await ctx.send(file=discord.File(image_data, filename=filename))
                     else:
                         await ctx.send("Image not found.")
+        except aiohttp.ClientError as e:
+            await ctx.send("An error occurred while fetching the image.")
         except Exception as e:
             await ctx.send(f"An error occurred: {e}")
