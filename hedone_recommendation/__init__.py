d70e78ac39429bfddfd0516ba59b9aa900a9df2b
DragoPrime <99618256+DragoPrime@users.noreply.github.com>
2025-06-16 20:26:20 +0300

    Update __init__.py

--- hedone_recommendation/__init__.py
+++ hedone_recommendation/__init__.py
@@ -1,4 +1,4 @@
-from .hedone_recommendation import JellyfinRecommendation
+from .hedone_recommendation import HedoneRecommendation
 
 async def setup(bot):
-    await bot.add_cog(JellyfinRecommendation(bot))
+    await bot.add_cog(HedoneRecommendation(bot))
